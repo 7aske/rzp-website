47d70dc5e12e0894540df06ff51deee37455987e
Nikola Tasic <ntasic7@gmail.com>
2020-08-27 19:25:26 +0200

    NODE_ENV url configs

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,4 @@
-export const blogUrl = "http://blog.rzp.7aske.xyz";
-export const backendUrl = "http://7aske.xyz:8080/rzp-blog"
-// export const blogUrl = "http://localhost:3001";
-// export const backendUrl = "http://localhost:8080/rzp-blog";
+export const blogUrl = process.env.NODE_ENV === "development" ?
+	"http://localhost:3000" : "http://blog.rzp.7aske.xyz";
+export const backendUrl = process.env.NODE_ENV === "development" ?
+	"http://localhost:8080/rzp-blog" : "http://7aske.xyz:8080/rzp-blog";

420b1207f40f59cea93abfad342150b90d08b161
Nikola Tasic <ntasic7@gmail.com>
2021-04-15 12:10:20 +0200

    header code, theme colors

--- src/pages/header/Header.tsx
+++ src/pages/header/Header.tsx
@@ -5,27 +5,35 @@ import localization from "./localization";
 import { BlogPostList } from "./blog/BlogPostList";
 import * as blogService from "../../services/blogService";
 import { Row, Col, Container } from "react-materialize";
+import { Code } from "./code/Code";
 
 export const Header = () => {
 	const [locale] = useLocale();
 
 	return (
 		<section id="header">
-			<Container>
-				<Row>
-					<Col s={12} m={12} l={7} className="animate__animated animate__slideInLeft animate__fast">
-						<h4 className="title">
-							{localization[locale].title}
-						</h4>
-						<p className="text">
-							{localization[locale].text}
-						</p>
-					</Col>
-					<Col s={12} m={12} l={5}>
-						<BlogPostList title={localization[locale].latestPosts} service={blogService.getLatestPosts}/>
-					</Col>
-				</Row>
-			</Container>
+			<div>
+				<Container>
+					<Code/>
+				</Container>
+			</div>
+			<div className="content">
+				<Container>
+					<Row>
+						<Col s={12} m={12} l={7} className="animate__animated animate__slideInLeft animate__fast">
+							<h4 className="title">
+								{localization[locale].title}
+							</h4>
+							<p className="text">
+								{localization[locale].text}
+							</p>
+						</Col>
+						<Col s={12} m={12} l={5}>
+							<BlogPostList title={localization[locale].latestPosts} service={blogService.getLatestPosts}/>
+						</Col>
+					</Row>
+				</Container>
+			</div>
 		</section>
 	);
 };
---
+++ src/pages/header/code/Code.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import "./Code.scss"
+import Typist from "react-typist";
+
+export const Code = () => {
+	return (
+		<div id="code">
+			{/*<Typist cursor={{element: "█", blink: true}} avgTypingDelay={30}>*/}
+			{/*</Typist>*/}
+		</div>
+	);
+}

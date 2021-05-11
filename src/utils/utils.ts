b884096a53d1ea8eec25478d3dc619f132dc0dab
Nikola Tasic <ntasic7@gmail.com>
2021-05-11 13:52:41 +0200

    scroll offset fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,12 +6,14 @@ export const scrollToTop = (ev?: React.MouseEvent<any, any>) => {
 };
 
 export const scrollTo = (ev?: React.MouseEvent<any, any>) => {
+	const nav = document.querySelector("nav");
+	const offset = nav ? 60 : 0;
 	if (!ev) return;
 	ev.preventDefault();
 	const url = new URL((ev.target as HTMLAnchorElement).href);
 	const elemId = url.hash;
-	const elem = document.querySelector(elemId);
+	const elem = document.querySelector(elemId) as HTMLElement;
 	console.log(elemId, elem);
 	if (!elem) return;
-	elem.scrollIntoView({behavior: "smooth"});
+	window.scroll({top: elem.offsetTop - offset, behavior:"smooth"})
 };
